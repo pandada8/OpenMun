97a18ab75d01945b3bb9ab9fc355bc81bc52e4e3
pandada8 <pandada8@gmail.com>
2014-07-14 14:04:02 +0800

    初步完成新建动议模块

---
+++ controller/MotionNew.js
@@ -0,0 +1,41 @@
+// TODO 使用统一的Motion结构体
+
+var mun = angular.module('mun')
+mun.controller('MotionNew',["$scope",'$location',function($scope){
+  $scope.motion_type = {
+    1:'自由磋商',
+    2:'有主持核心磋商',
+    3:'更改投票顺序',
+    4:'取消延置决议草案',
+    5:'修改正式辩论发言时间',
+    6:'暂时休会',
+    7:'延置决议草案',
+    8:'结束正式辩论'
+  }
+
+
+
+  $scope.click = function(num){
+
+    $scope.now = num
+    $scope._type = $scope.motion_type[num]
+    if (num >= 7 ){
+      $scope.people_require = "三分之二多数"
+      // FIXME : 与Rollcall 结合实时显示需要人数
+    }else{
+      $scope.people_require = "简单多数"
+    }
+    // FIXME 剩下的逻辑
+  }
+  $scope.back = function(){
+    // TODO 抓取数据并存储
+    d = {}
+
+    // 返回 /motion 页
+    $location.path('/motion')
+  }
+
+  // 进行初始化
+  $scope.click(1)
+
+}])
--- controller/main.js
+++ controller/main.js
@@ -10,10 +10,30 @@ mun.config(['$routeProvider','$locationProvider',function($routeProvider,$locati
 
 	}).when('/about',{
 		templateUrl:"about.html",
-		controller:'About'
+		controller:'AboutController'
+	}).when('/motion/new',{
+		templateUrl:"motion_new.html",
+		controller:'MotionNew'
 	})
 }])
 
-mun.controller('About', ['$scope', function($scope){
+mun.controller("MenuCon",["$scope",'$location',function($scope,$location){
+	$scope.menu = {
+		1:['点名','/rollcall'],
+		2:['主发言名单','/speaklist'],
+		3:['动议','/motion'],
+		4:['投票','/vote'],
+		5:['统计','/statistic'],
+		6:['设置','/setting'],
+		7:['关于','/about'],
+	}
+	$scope.click = function(num){
+		$scope.active = num
+		$location.path($scope.menu[num][1])
+	}
+
+}])
+
+mun.controller('AboutController', ['$scope', function($scope){
 	$scope.ver = VERSION
 }])
